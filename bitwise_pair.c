5434fcf3342764988fc5e601b2ce2016688da765
abin <abinjn18@gmail.com>
2020-05-03 00:27:30 +0530

    devika

--- bitwise_pair.c
+++ bitwise_pair.c
@@ -2,16 +2,27 @@
 int main()
 {
     int arr[]={10,7,2,8,3};
-    int i,j,res;
+    int i,j,res,flag;
     for(i = 0; i < 5; i++)
     {
         for(j = i+1; j < 5; j++ )
         {
             res = arr[i] & arr[j];
-            if(res % 2 ==  0)
+            //printf("   %d   ",res);
+            flag = 0;
+            while(res > 2)
+            {
+                if(res % 2 != 0)
+                {
+                    flag = 1;
+                    break;
+                }
+                res = res / 2;
+            }
+            if(res == 2 && flag == 0)
             {
                 printf("%d %d\n",arr[i],arr[j]);
-                fdf
+            
             }
         }
     }

23e7220f9e2e027c64e008671f6616cc4d1acceb
abin <abinjn18@gmail.com>
2020-05-02 21:55:37 +0530

    commit

---
+++ bitwise_pair.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int arr[]={10,7,2,8,3};
+    int i,j,res;
+    for(i = 0; i < 5; i++)
+    {
+        for(j = i+1; j < 5; j++ )
+        {
+            res = arr[i] & arr[j];
+            if(res % 2 ==  0)
+            {
+                printf("%d %d\n",arr[i],arr[j]);
+            }
+        }
+    }
+}
